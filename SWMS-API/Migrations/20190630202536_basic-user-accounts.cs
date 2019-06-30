e354f4b5fb655d79f94644a55ceaa4a92de19eeb
Raphael Schmitz <raphaelschmitz00@gmail.com>
2019-06-30 22:26:21 +0200

    adding migration

---
+++ SWMS-API/Migrations/20190630202536_basic-user-accounts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SwmsApi.Migrations
+{
+    public partial class basicuseraccounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
